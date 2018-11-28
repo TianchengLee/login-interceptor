8e8965edf463c7303a849ceb49d2371d6ebd4bdd
TianchengLee <ltc6634284@gmail.com>
2018-11-28 23:54:53 +0800

    添加路由全局守卫

--- src/main.js
+++ src/main.js
@@ -10,6 +10,27 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
+router.beforeEach((to, from, next) => {
+  let token = localStorage.getItem('token')
+  if (!token && to.path !== '/login') {
+    return next({ path: '/login' })
+  }
+  next()
+  // let token = localStorage.getItem('token')
+  // // 如果已经登录，那我不干涉你，让你随便访问
+  // if (token) {
+  //   next()
+  // } else {
+  //   if (to.path !== '/login') {
+  //     // 如果没有登录，但你访问其他需要登录的页面，那我就让你跳到登录页面去
+  //     next({path: '/login'})
+  //   } else {
+  //     // 如果没有登录，但你访问的login，那就不干涉你，让你访问
+  //     next()
+  //   }
+  // }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
